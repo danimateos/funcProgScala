ec9a853b8368fdf0c32e991db28160df17340794
danimateos <dmateos.sanmartin@gmail.com>
2014-11-07 18:13:38 +0100

    exercises 2.3 -2.5

--- src/main/scala/Chapter_02.scala
+++ src/main/scala/Chapter_02.scala
@@ -20,5 +20,16 @@ object Chapter_02 {
     else isSorted(as.tail, ordered)
   }
 
+  def curry[A, B, C] (f: (A, B) => C): A => (B => C) = {
+    a => f(a, _)
+  }
+
+  def uncurry[A, B, C] (f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)    
+  }
+
+  def compose[A, B, C] (f: B => C, g: A => B): A => C = {
+    x: A => f(g(x))
+  }
 
 }
