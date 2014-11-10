30841afda1be8f601345b85f7efb18e60cdace3f
danimateos <dmateos.sanmartin@gmail.com>
2014-11-10 14:48:30 +0100

    Chapter 3 exercises through to 3.9

--- src/main/scala/Chapter_03.scala
+++ src/main/scala/Chapter_03.scala
@@ -3,4 +3,42 @@ package com.mateos.chiusano
 
 object Chapter_03 {
 
+  def tail[A](as: List[A]): List[A] = as match {
+    case x :: xs => xs
+    case Nil => Nil
+  }
+
+  def setHead[A] (a: A, as: List[A]): List[A] = as match {
+    case x :: xs => a :: xs
+    case _       => Nil
+  }
+
+  @annotation.tailrec
+  def drop[A] (l: List[A], n: Int): List[A] = n match {
+    case 0 => l
+    case x => drop(l.tail, n-1)
+  }
+
+  @annotation.tailrec
+  def dropWhile[A] (l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case x :: xs if !f(x) => x :: xs
+    case x :: xs if f(x)  => dropWhile(xs, f)
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case x :: Nil => Nil
+    case x :: xs => x :: init(xs)
+  }
+
+  def foldRight[A,B] (as: List[A], z: B) (f: (A, B) => B): B = as match {
+    case Nil => z
+    case x :: xs => f(x, foldRight(xs, z) (f))
+
+  }
+
+  def length[A] (l: List[A]): Int = foldRight(l, 0) ((x, y) => y + 1)
+
+
+
 }
--- src/test/scala/Chapter_03_Spec.scala
+++ src/test/scala/Chapter_03_Spec.scala
@@ -4,10 +4,37 @@ import com.mateos.chiusano.Chapter_03
 
 object Chapter_03_Spec extends Specification {
 
-  "Something" should {
+  val list1 = 1 :: 3 :: 4 :: 5 :: 6 :: Nil
+  val list2 = 3 :: 2 :: 1 :: 5 :: 6 :: Nil
+  val list3 = 6 :: 5 :: 4 :: 3 :: 2 :: Nil
 
-    "do something" in {
-      done
+  "The tail() method" should {
+    "work" in {
+      Chapter_03.tail(list1) mustEqual 3 :: 4 :: 5 :: 6 :: Nil
+      //Chapter_03.tail(list2) mustEqual 2 :: 1 :: 5 :: 6 :: Nil
+      //Chapter_03.tail(list3) mustEqual 5 :: 4 :: 3 :: 2 :: Nil
     }
-  
+  }
+
+  "The setHead() method" should {
+    "work" in {
+      Chapter_03.setHead(2, list1) mustEqual 2 :: 3 :: 4 :: 5 :: 6 :: Nil
+      Chapter_03.setHead(2, list2) mustEqual 2 :: 2 :: 1 :: 5 :: 6 :: Nil
+    }
+  }
+
+  "The drop() method" should {
+    "return Nil" in {
+      Chapter_03.drop(list1, 5) mustEqual Nil
+    }
+    "drop n" in {
+      Chapter_03.drop(list1, 3).length mustEqual list1.length-3
+    }
+  }
+
+  "The dropWhile() method" should {
+    "work" in {
+      Chapter_03.dropWhile(list3, (x: Int) => x > 3) mustEqual 3 :: 2 :: Nil
+    }
+  }
 }
