43287c4e48b058bd4af86f56b8dc2e6038d2e729
danimateos <dmateos.sanmartin@gmail.com>
2014-11-10 22:42:20 +0100

    Exercises 3.25, 3.26, 3.27, 3.28

--- src/main/scala/Chapter_03.scala
+++ src/main/scala/Chapter_03.scala
@@ -106,6 +106,30 @@ object Chapter_03 {
     case (x :: xs, y :: ys) => f(x, y) :: zipWith(xs, ys) (f)
   }
 
-//  def hasSubsequence
+//  def hasSubsequence[A] (sup: List[A], sub: List[A]): Boolean =
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + size(r) + 1
+  }
+
+  def maximum (t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A] (t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => depth(l) max depth(r) + 1
+  }
+
+  def map[A,B] (t: Tree[A]) (f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
 }
