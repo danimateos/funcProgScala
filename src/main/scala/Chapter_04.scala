fb04377f0541375fe22dd680cf8715b38ad77e68
danimateos <dmateos.sanmartin@gmail.com>
2014-11-30 19:26:53 +0100

    Exercise 4.1: base implementation of Option methods

--- src/main/scala/Chapter_04.scala
+++ src/main/scala/Chapter_04.scala
@@ -3,9 +3,35 @@ package com.mateos.chiusano
 
 object Chapter_04 {
 
-  sealed trait Option[+A]
-  case class Some[+A](get: A) extends Option[A]
+  sealed trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(thing) => Some(f(thing))
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+      case None => None
+      case Some(thing) => f(thing)
+    }
+    
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(thing) => thing
+    }
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] = if (this == None) ob else this
+    
+    def filter(f: A => Boolean): Option[A] = this match {
+      case None => None
+      case Some(thing) => if (f(thing)) this else None
+    }
+
+
+  }
+
   case object None extends Option[Nothing]
+  case class Some[+A](get: A) extends Option[A]
+
 
 
 }
